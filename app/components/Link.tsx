901597eda29a666de2aa94b1090ad416e46bad76
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-08-30 13:27:11 -0600

    build link styled component

---
+++ app/components/Link.tsx
@@ -0,0 +1,16 @@
+import NextLink from 'next/link'
+import { Link as RadixLink } from '@radix-ui/themes'
+import React from 'react'
+
+type Props = {
+  href: string
+  children: string
+}
+
+export default function Link({ href, children }: Props) {
+  return (
+    <NextLink href={href} passHref legacyBehavior>
+      <RadixLink>{children}</RadixLink>
+    </NextLink>
+  )
+}
