f3651b9358cee88d4968d73590b8205f22476de5
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-08-30 20:38:01 -0600

    add login skeletin

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,6 +6,7 @@ import { AiFillBug } from 'react-icons/ai'
 import classNames from 'classnames'
 import { useSession } from 'next-auth/react'
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes'
+import { Skeleton } from './components'
 
 export default function NavBar() {
   return (
@@ -46,7 +47,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession()
 
-  if (status === 'loading') return null
+  if (status === 'loading') return <Skeleton width="3rem" />
   if (status === 'unauthenticated') return <Link className='nav-link' href="/api/auth/signin">Login</Link>
 
   return <Box>
