919b6e46b8e9fe25b16b43b5ba76c285f4b127fe
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-08-31 06:33:22 -0600

    enhance api to assign users to issues

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -4,3 +4,22 @@ export const issueSchema = z.object({
   title: z.string().min(1).max(255, 'Title is required'),
   description: z.string().min(1, 'Description is required'),
 })
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1)
+    .max(255, 'Title is required')
+    .optional(),
+  description: z
+    .string()
+    .min(1, 'Description is required')
+    .max(65535, 'Description is too long')
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, 'Assigned to user is required')
+    .max(255)
+    .optional()
+    .nullable(),
+})
